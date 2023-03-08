eb247920ab71d9e895a3fc0e9298788d897bf9d1
taylor <2116262647@qq.com>
2023-03-08 16:03:02 +0800

    all

---
+++ homework/homeworkSecond/ConsoleApp1/primeFactor.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleApp1
+{
+    internal class primeFactor
+    {
+        static void getPrimeFactor(int n)
+        {
+
+            for (int i = 1; i < n + 1; i++)
+            {
+                int count = 0;
+                for (int j = 1; j < i + 1; j++)
+                {
+                    if (count > 2)
+                    {
+                        break;
+                    }
+                    if (n % i == 0 && i % j == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2)
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            //1.指定数据的所有素数因子
+
+            //try{}catch{}语句抓住异常并抛出
+            //写代码先做合法性验证
+
+            try
+            {
+                Console.Write("请输入一个正整数：");
+                int n = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("该数的所有素数因子为：");
+                getPrimeFactor(n);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"发生异常：{e.Message}");
+            }
+
+        }
+    }
+}
---
+++ homework/homeworkSecond/ehrlichSieve/ehrlichSieve.cs
@@ -0,0 +1,32 @@
+﻿namespace ehrlichSieve
+{
+    internal class ehrlichSieve
+    {
+        static void getEhrlichSieve(int n)
+        {
+            int[] notSu = new int[n+1];
+            for (int i = 2; i*i <= n; i++)    //i<11可以写为i*i<=100
+            {
+                for (int j = i; j < (n + 1) / i + 1; j++)
+                {
+                    notSu[i * j] = 1;
+                }
+            }
+
+            for (int i = 2; i < n + 1; i++)
+            {
+                if (notSu[i] == 0)
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            //3.用“埃氏筛法”求2~ 100以内的素数。2~ 100以内的数，先去掉2的倍数，再去掉3的倍数，再去掉4的倍数，以此类推...最后剩下的就是素数。
+            int n = 100;
+            getEhrlichSieve(n);
+        }
+    }
+}

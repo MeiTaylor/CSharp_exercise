bcaa78c8562950184a56fc4278afc42b6bc79d09
taylor <2116262647@qq.com>
2023-03-19 22:22:03 +0800

    all

---
+++ homework/homeworkFourth/Event/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+/*
+ 使用事件机制，模拟实现一个闹钟功能。
+闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。
+在闹钟走时时或者响铃时，在控制台显示提示信息。
+ */
+
+
+namespace Event
+{
+    public delegate void AlarmClockEvent(System.DateTime time);
+
+    public class AlarmClock
+    {
+        private System.DateTime time;
+        public System.DateTime Time
+        {
+            get { return time; }
+            set { time = System.DateTime.Now; }
+        } 
+
+        public System.DateTime setAlarmClock(int year, int month, int day, int hour, int minute, int second)
+        {
+            return System.DateTime.Parse($"{year}-{month}-{day} {hour}:{minute}:{second}");
+        }
+
+        public event  AlarmClockEvent alarmClockEvent = null;
+        public void run(System.DateTime alarmTime)
+        {
+            while( alarmClockEvent != null )
+            {                
+
+
+
+                Thread.Sleep(1000);
+                TimeSpan ts = System.DateTime.Now - alarmTime;
+
+                alarmClockEvent(alarmTime);
+
+                if (ts.Seconds >= 0 && ts.Seconds <= 1)
+                {
+                    break; // 退出循环
+                }
+
+
+            }
+        }
+
+
+    }
+
+
+    internal class Program
+    {
+        static void Tick(System.DateTime alarmTime)
+        {
+            Console.WriteLine($"滴答滴答，现在是时间为{System.DateTime.Now}");
+
+        }
+
+        //static void Alarm(System.DateTime alarmTime)
+        //{
+        //    if (System.DateTime.Now == alarmTime)
+        //    {
+        //        Console.WriteLine($"闹钟到啦，设定的时间{alarmTime}到啦");
+        //    }
+        //}
+
+
+        static void Alarm(System.DateTime alarmTime)
+        {
+            TimeSpan ts = System.DateTime.Now - alarmTime;
+            if (ts.Seconds >= 0 && ts.Seconds <= 1)
+            {
+                Console.WriteLine($"闹钟到啦，设定的时间{alarmTime}到啦");
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            AlarmClock alarmClock = new AlarmClock();
+
+            System.DateTime alarmTime = alarmClock.setAlarmClock(2023, 3, 19, 22, 4, 50);
+            Console.WriteLine($"闹钟时间为：{alarmTime}");
+
+            alarmClock.alarmClockEvent += Tick;
+            alarmClock.alarmClockEvent += Alarm;
+
+            alarmClock.run(alarmTime);
+
+
+
+
+
+        }
+    }
+}
---
+++ homework/homeworkFourth/homeworkFourth/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace homeworkFourth
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T Data)
+        {
+            Next = null;
+            this.Data = Data;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public Node<T> Head { get { return head; } }
+        public GenericList() {
+            tail = null;
+            head = null;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach (Action<T> action)
+        {
+            Node<T> i = this.Head;
+            while(i != null)
+            {
+                action(i.Data);
+                i = i.Next;
+            }
+        }
+    }
+
+    //ForEach(Action<T> action)
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello, World!");
+                //首先先创建一个链表
+                GenericList<double> list = new GenericList<double>();
+                list.Add(3.1);
+                list.Add(2.9);
+                list.Add(4.5);
+                //先给最大最小值赋初值
+                double max = list.Head.Data;
+                double min = list.Head.Data;
+                double sum = 0;
+                list.ForEach(x => { if (x > max) max = x; });
+                list.ForEach(x => { if (x < min) min = x; });
+                list.ForEach(x => { sum += x; });
+                //Console.WriteLine(max);
+                Console.WriteLine($"最大值为{max}");
+                Console.WriteLine($"最小值为{min}");
+                Console.WriteLine($"总和为{sum}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"错误信息为{e}");
+            }
+
+        }
+    }
+}
